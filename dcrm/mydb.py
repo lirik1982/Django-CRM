e96a1eb2e5b85d258a68dedb4a8e6ec899e63fc3
KirillDegtyarev <lirik19@yandex.ru>
2023-03-12 17:20:12 +0700

    'Initial'

---
+++ dcrm/mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'fight'
+)
+
+cursorObject = database.cursor()
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print('all done!')
