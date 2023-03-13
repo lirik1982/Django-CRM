0ad92bca656601eb023f58c5646dd2eb9fd6a20b
KirillDegtyarev <lirik19@yandex.ru>
2023-03-13 16:13:24 +0700

    admin done

--- dcrm/mydb.py
+++ dcrm/mydb.py
@@ -1,13 +1,14 @@
-import mysql.connector
-
-database = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = 'fight'
-)
-
-cursorObject = database.cursor()
-
-cursorObject.execute("CREATE DATABASE elderco")
-
-print('all done!')
+# for migrate
+# import mysql.connector
+#
+# database = mysql.connector.connect(
+#     host = 'localhost',
+#     user = 'root',
+#     passwd = 'fight'
+# )
+#
+# cursorObject = database.cursor()
+#
+# cursorObject.execute("CREATE DATABASE elderco")
+#
+# print('all done!')
